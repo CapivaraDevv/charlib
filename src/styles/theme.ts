c968b63d7f6b60accdd98fe1c5ff375c7c9f7e73
Pedro <pedro.henrique.pereira.2014@gmail.com>
2026-06-30 10:53:36 -0300

    chore: initial project setup

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const Theme = {
+  colors: {
+    background: "#5B3E2B",
+    surface: "#674533",
+    surfaceDark: "#3E281D",
+
+    primary: "#FFFFFF",
+
+    secondary: "#E6D7C8",
+
+    accent: "#D9A15B",
+
+    success: "#7FA36B",
+  },
+};
