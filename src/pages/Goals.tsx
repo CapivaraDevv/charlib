8eb8ed35339f5645139ba545183226a0e6be312d
Pedro <pedro.henrique.pereira.2014@gmail.com>
2026-07-23 15:23:51 -0300

    feat: restructure Goals component layout; add navigation button for returning home

--- src/pages/Goals.tsx
+++ src/pages/Goals.tsx
@@ -2,17 +2,29 @@ import DailyGoalCard from "../components/goals/DailyGoalCard";
 import MonthlyGoalCard from "../components/goals/MonthlyGoalCard";
 import ReadingCardStreak from "../components/goals/ReadingCardStreak";
 import WeeklyGoalCard from "../components/goals/WeeklyGoalCard";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/common/Button";
 
 export default function Goals() {
+  const navigate = useNavigate();
+
   return (
     <div className="mx-auto w-full max-w-7xl space-y-10 px-4 py-6">
-      <h1 className="crimson-text-regular text-4xl font-bold tracking-wide">
-        Seus objetivos
-      </h1>
+      <div className="flex flex-row justify-between">
+        <h1 className="crimson-text-regular text-4xl font-bold tracking-wide">
+          Seus objetivos
+        </h1>
+        <Button
+            onClick={() => navigate("/")}
+            variant="primary"
+          >
+            Voltar
+          </Button>
+      </div>
 
       <div className="grid gap-6 lg:grid-cols-2">
         <DailyGoalCard />
-        
+
         <ReadingCardStreak />
 
         <WeeklyGoalCard />
